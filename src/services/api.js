0a4b3a08aebe2ac2061244170a9ba27acbe13580
jaseemjaseemn523 <jaseemjaseemn523@gmail.com>
2026-08-30 23:51:40 +0530

    Update baseURL to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-
 api.interceptors.request.use((config) => {
-
   if (config.url && (config.url.includes('accounts/register/') || config.url.includes('accounts/login/'))) {
     return config;
   }
